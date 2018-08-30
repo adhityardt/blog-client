b4a2ebaf078a3b56f0070781288204df291baf3e
Adhitya Rahman <rahmanadhitya2201@gmail.com>
2018-08-30 14:27:27 +0700

    Delete API Key

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import Firebase from 'firebase'
 
 const firebaseApp = Firebase.initializeApp({
   // Populate your firebase configuration data here.
-  apiKey: 'AIzaSyDTUok54H8dglMjql8OWWxXaJ5HXjmIIyg',
-  authDomain: 'blog-d4266.firebaseapp.com',
-  databaseURL: 'https://blog-d4266.firebaseio.com',
-  projectId: 'blog-d4266',
-  storageBucket: 'blog-d4266.appspot.com',
-  messagingSenderId: '545102028382'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
 })
 
 // Export the database for components to use.
